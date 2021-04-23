f8f48e2f22c016b10d61aa708ba6290b53bbec84
wangyuxiang123 <1105017602@qq.com>
2021-04-23 20:10:50 +0800

    feat: 增加修改功能

--- group-17/src/LibraryOperator.java
+++ group-17/src/LibraryOperator.java
@@ -1,7 +1,20 @@
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
 public class LibraryOperator {
+    Scanner scanner = new Scanner(System.in);
+    InitializeBooks map = new InitializeBooks();
     //浏览
     public void scanBooks() {
-
+        if(!map.books.isEmpty()) {
+            Iterator it = map.books.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry obj = (Map.Entry) it.next();
+                System.out.println("ID: "+obj.getKey()+" 书名："+obj.getValue());
+            }
+        }
     }
     //增加
     public void addBook(){
@@ -13,7 +26,17 @@ public class LibraryOperator {
     }
     //修改
     public void modBook(){
+        System.out.print("请输入需要修改的书ID：");
+        String ID = scanner.next();
+        System.out.print("请输入需要修改的书名：");
+        String name = scanner.next();
 
+        if(map.books.containsKey(ID)){
+            map.books.put(ID,name);
+            System.out.println("修改成功");
+        }else{
+            System.out.println("没有找到此书");
+        }
     }
     //推荐
     public void redBook(){
