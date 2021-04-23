a2983480acc51642fb4021da5d5fe3128a92ab1a
wangyuxiang123 <1105017602@qq.com>
2021-04-23 17:52:10 +0800

    refactor: 重构

---
+++ group-17/src/InitializeBooks.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InitializeBooks {
+    HashMap<String, String > books = new HashMap<String, String>() {{
+        put("1","平凡的世界");
+        put("2","穆斯林的葬礼");
+        put("3","挪威的森林");
+        put("4","基督山伯爵");
+        put("5","教父");
+        put("6","苏菲的世界");
+        put("7","麦田里的守望者");
+    }};
+
+}
---
+++ group-17/src/LibraryOperator.java
@@ -0,0 +1,19 @@
+public class LibraryOperator {
+    public void scanBooks() {
+
+    }
+    public void sddBook(){
+
+    }
+
+    public void delBook(){
+
+    }
+
+    public void modBook(){
+
+    }
+    public void redBook(){
+
+    }
+}
