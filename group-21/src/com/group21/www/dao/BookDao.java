a7ab36b2697f89da97fc23803c80d2e2b0f88d1e
lcwcool <1909944530@qq.com>
2021-04-25 23:58:25 +0800

    rewrite the book lookup function

--- group-21/src/com/group21/www/dao/BookDao.java
+++ group-21/src/com/group21/www/dao/BookDao.java
@@ -30,13 +30,84 @@ public class BookDao {
         return null;
     }
 
-    public List<Book> findBookByBookName(String bookName) {
-        return null;
+    /**
+     * 根据书名查找图书
+     *
+     * @param bookName 图书名
+     * @return 符合要求的图书集合
+     * @author 陈烨
+     */
+    public List<Book> findBookByBookName(String bookName){
+        ArrayList<Book> books = new ArrayList<>();
+        String queryStr = "select * from book where bookname = ?";
+        Connection connection = DBconnection.getConnection();
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement(queryStr);
+            preparedStatement.setString(1,bookName);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                books.add(new Book(resultSet.getString("bookname"),resultSet.getString("authorname"),resultSet.getBoolean("state")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return books;
     }
 
-
-    public List<Book> findBookByAuthorName(String authorName) {
-        return null;
+    /**
+     * 根据作者名查找图书
+     *
+     * @param authorName 作者名
+     * @return 符合要求的图书集合
+     * @author 陈烨
+     */
+    public List<Book> findBookByAuthorName(String authorName){
+        ArrayList<Book> books = new ArrayList<>();
+        String queryStr = "select * from book where authorname = ?";
+        Connection connection = DBconnection.getConnection();
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement(queryStr);
+            preparedStatement.setString(1,authorName);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                books.add(new Book(resultSet.getString("bookname"),resultSet.getString("authorname"),resultSet.getBoolean("state")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return books;
     }
 
     public boolean updateState(String bookName, String authorName) {
