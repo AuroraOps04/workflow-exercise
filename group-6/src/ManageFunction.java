96c32eed629d40c70e83fab5d7b8a20abd2e56e0
Jrz2000 <308335266@qq.com>
2021-04-25 12:54:33 +0800

    Update java programs.

--- group-6/src/ManageFunction.java
+++ group-6/src/ManageFunction.java
@@ -25,15 +25,15 @@ public class ManageFunction {
     public static Book[] DeleteBook(Book[] books) {
         System.out.println("查看现在图书：");
         ManageFunction.ShowBook(books);
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("请输入要删除的图书序号：");
-        int n=scanner.nextInt();
-        Book[] newbooks=new Book[books.length-1];
-        for (int i = 0,j=0; i < books.length; i++) {
-            if (i!=n-1){
-                newbooks[j]=books[i];
-                newbooks[j].setBooknum(j+1);
+        int n = scanner.nextInt();
+        Book[] newbooks = new Book[books.length - 1];
+        for (int i = 0, j = 0; i < books.length; i++) {
+            if (i != n - 1) {
+                newbooks[j] = books[i];
+                newbooks[j].setBooknum(j + 1);
                 j++;
             }
         }
@@ -42,19 +42,14 @@ public class ManageFunction {
 
     //    图书查看
     public static void ShowBook(Book[] books) {
-        System.out.println("请输入需要搜索的书名");
-        Scanner scanner = new Scanner(System.in);
-        String a = scanner.next();
         for (int i = 0; i < books.length; i++) {
-            if (a.equals(books[i].getBookName())) {
-                System.out.println(" booknum: "+books[i].getBooknum()+" 书名 "+books[i].getBookName()+" 作者 "+books[i].getWriter());
-            }
-
+            System.out.println(" 书籍编号：" + books[i].getBooknum() + " 书籍名：" + books[i].getBookName() + " 作者名：" + books[i].getWriter());
         }
     }
+
     //    图书修改
     public static Book[] ChangeBook(Book[] books) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         //先显示全部数据供查看
         ManageFunction.ShowBook(books);
         System.out.print("请输入你要修改的图书id：");
