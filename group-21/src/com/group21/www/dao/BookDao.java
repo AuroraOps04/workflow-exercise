df7267e6c4259fcd294a3aec11b6c33bc1eb92a6
lcwcool <1909944530@qq.com>
2021-04-26 13:15:30 +0800

    delete book function

--- group-21/src/com/group21/www/dao/BookDao.java
+++ group-21/src/com/group21/www/dao/BookDao.java
@@ -11,13 +11,13 @@ import java.util.Random;
 
 public class BookDao {
 
-       /**
+    /**
      * 获取所有图书信息
      *
      * @return 图书集合
      * @author 王康靖
      */
-    public List<Book> getTotalBooks(){
+    public List<Book> getTotalBooks() {
         ArrayList<Book> books = new ArrayList<>();
         String queryStr = "select * from book";
         Connection connection = DBconnection.getConnection();
@@ -26,12 +26,12 @@ public class BookDao {
         try {
             preparedStatement = connection.prepareStatement(queryStr);
             resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
-                books.add(new Book(resultSet.getString("bookname"),resultSet.getString("authorname"),resultSet.getBoolean("state")));
+            while (resultSet.next()) {
+                books.add(new Book(resultSet.getString("bookname"), resultSet.getString("authorname"), resultSet.getBoolean("state")));
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             if (connection != null) {
                 try {
                     connection.close();
@@ -58,7 +58,6 @@ public class BookDao {
     }
 
 
-
     /**
      * 添加图书
      *
@@ -67,7 +66,7 @@ public class BookDao {
      * @return 增加图书是否成功
      * @author 陈棋
      */
-    public boolean addBook(Book book){
+    public boolean addBook(Book book) {
         String addBookStr = "insert into book(bookname,authorname,state) values(?,?,?)";
         Connection connection = DBconnection.getConnection();
         PreparedStatement preparedStatement = null;
@@ -76,16 +75,16 @@ public class BookDao {
             preparedStatement = connection.prepareStatement(addBookStr);
             preparedStatement.setString(1, book.getBookName());
             preparedStatement.setString(2, book.getAuthorName());
-            preparedStatement.setBoolean(3,book.isState());
+            preparedStatement.setBoolean(3, book.isState());
             int res = preparedStatement.executeUpdate();
             if (res > 0) {
                 result = true;
-            }else {
+            } else {
                 result = false;
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             if (connection != null) {
                 try {
                     connection.close();
@@ -105,8 +104,48 @@ public class BookDao {
     }
 
 
+    /**
+     * 删除图书
+     *
+     * @param bookName   图书名
+     * @param authorName 作者名
+     * @return
+     * @author 林朝玮
+     */
     public boolean delBook(String bookName, String authorName) {
-        return false;
+        String delBookStr = "delete from book where bookname = ? and authorname = ?";
+        Connection connection = DBconnection.getConnection();
+        PreparedStatement preparedStatement = null;
+        boolean result = false;
+        try {
+            preparedStatement = connection.prepareStatement(delBookStr);
+            preparedStatement.setString(1, bookName);
+            preparedStatement.setString(2, authorName);
+            int res = preparedStatement.executeUpdate();
+            if (res > 0) {
+                result = true;
+            } else {
+                result = false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
     }
 
     /**
@@ -115,13 +154,13 @@ public class BookDao {
      * @return 推荐的图书的集合
      * @author 蔡云龙
      */
-    public List<Book> recommendedBooks(){
+    public List<Book> recommendedBooks() {
         List<Book> totalBooks = getTotalBooks();
         ArrayList<Book> books = new ArrayList<>();
         HashSet<Integer> integers = new HashSet<>();
         Random random = new Random();
         int cnt = 0;
-        while (cnt == 0){
+        while (cnt == 0) {
             cnt = random.nextInt(totalBooks.size());
         }
         while (integers.size() < cnt) {
@@ -141,7 +180,7 @@ public class BookDao {
      * @return 符合要求的图书集合
      * @author 陈烨
      */
-    public List<Book> findBookByBookName(String bookName){
+    public List<Book> findBookByBookName(String bookName) {
         ArrayList<Book> books = new ArrayList<>();
         String queryStr = "select * from book where bookname = ?";
         Connection connection = DBconnection.getConnection();
@@ -149,14 +188,14 @@ public class BookDao {
         ResultSet resultSet = null;
         try {
             preparedStatement = connection.prepareStatement(queryStr);
-            preparedStatement.setString(1,bookName);
+            preparedStatement.setString(1, bookName);
             resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
-                books.add(new Book(resultSet.getString("bookname"),resultSet.getString("authorname"),resultSet.getBoolean("state")));
+            while (resultSet.next()) {
+                books.add(new Book(resultSet.getString("bookname"), resultSet.getString("authorname"), resultSet.getBoolean("state")));
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             if (connection != null) {
                 try {
                     connection.close();
@@ -189,7 +228,7 @@ public class BookDao {
      * @return 符合要求的图书集合
      * @author 陈烨
      */
-    public List<Book> findBookByAuthorName(String authorName){
+    public List<Book> findBookByAuthorName(String authorName) {
         ArrayList<Book> books = new ArrayList<>();
         String queryStr = "select * from book where authorname = ?";
         Connection connection = DBconnection.getConnection();
@@ -197,14 +236,14 @@ public class BookDao {
         ResultSet resultSet = null;
         try {
             preparedStatement = connection.prepareStatement(queryStr);
-            preparedStatement.setString(1,authorName);
+            preparedStatement.setString(1, authorName);
             resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
-                books.add(new Book(resultSet.getString("bookname"),resultSet.getString("authorname"),resultSet.getBoolean("state")));
+            while (resultSet.next()) {
+                books.add(new Book(resultSet.getString("bookname"), resultSet.getString("authorname"), resultSet.getBoolean("state")));
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             if (connection != null) {
                 try {
                     connection.close();
@@ -230,7 +269,7 @@ public class BookDao {
         return books;
     }
 
-     /**
+    /**
      * 更新图书在架状态
      *
      * @param bookName   图书名
@@ -238,24 +277,24 @@ public class BookDao {
      * @return 更新状态是否成功
      * @author 曾国桢
      */
-    public boolean updateState(String bookName,String authorName){
+    public boolean updateState(String bookName, String authorName) {
         String updateStr = "update book set state = abs(state-1) where bookname = ? and authorname = ?";
         Connection connection = DBconnection.getConnection();
         PreparedStatement preparedStatement = null;
         boolean result = false;
         try {
             preparedStatement = connection.prepareStatement(updateStr);
-            preparedStatement.setString(1,bookName);
-            preparedStatement.setString(2,authorName);
+            preparedStatement.setString(1, bookName);
+            preparedStatement.setString(2, authorName);
             int res = preparedStatement.executeUpdate();
             if (res > 0) {
                 result = true;
-            }else{
+            } else {
                 result = false;
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             if (connection != null) {
                 try {
                     connection.close();
