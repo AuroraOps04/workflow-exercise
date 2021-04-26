143da8418e4cfe67a1b7644991354c1d629c5878
xiao-1005 <78087003+xiao-1005@users.noreply.github.com>
2021-04-26 14:17:48 +0800

    Update Shop.java-add function

--- group-19/src/Manage/Shop.java
+++ group-19/src/Manage/Shop.java
@@ -53,7 +53,25 @@ public class Shop {
 			}
 		}
 	}
-	
+
+	//新增BOOK
+	public void addBOOK() {
+		System.out.println("--->新增书籍");
+		System.out.print("请输入图书名称:");
+		String name = input.next();
+		int i = -1;
+		for (BOOK d : book) {
+			if (name.equals(d.getName())) {
+				System.out.println("图书已存在，不能再添加该书籍！");
+				i = book.indexOf(d);
+			}
+		}
+		if (i < 0) {
+			book.add(new BOOK(book.size() + 1, name, "可借", ""));
+			System.out.println("新增《" + name + "》成功!");
+		}
+	}
+
 	//删除BOOK
 	public void deleteBOOK() {
 		System.out.println("--->删除书籍");
