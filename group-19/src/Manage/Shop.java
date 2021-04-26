90cdcebccd1c6c9fe7c2488e6eb48ade1f15c3bd
Ruma20 <1322518778@qq.com>
2021-04-26 22:09:56 +0800

    “注GjhWork1

--- group-19/src/Manage/Shop.java
+++ group-19/src/Manage/Shop.java
@@ -76,21 +76,21 @@ public class Shop {
 	public void deleteBOOK() {
 		System.out.println("--->删除书籍");
 		System.out.print("请输入删除图书名称:");
-		String name = input.next();
-		int index = -1;
-		for (BOOK d : book) {
-			if (name.equals(d.getName())) {
-				index = book.indexOf(d);
-				if (d.getStatus().equals("已借出")) {
-					System.out.println("《" + name + "》" + "为借出状态，不能删除！");
+		String Bookname = input.next();
+		int BooKindex = -1;
+		for (BOOK de : book) {
+			if (Bookname.equals(de.getName())) {
+				BooKindex = book.indexOf(de);
+				if (de.getStatus().equals("已借出")) {
+					System.out.println("《" + Bookname + "》" + "为借出状态，不能删除！");
 				}
 			}
 		}
-		if (index < 0)
+		if (BooKindex < 0)
 			System.out.println("不存在该图书");
 		else {
-			book.remove(index);
-			System.out.println("删除《" + name + "》成功!");
+			book.remove(BooKindex);
+			System.out.println("删除《" + Bookname + "》成功!");
 		}
 	}
 	
