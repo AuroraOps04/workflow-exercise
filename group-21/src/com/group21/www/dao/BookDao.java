39572b383f83fde028be84d7cee6f1c47d7b4faa
211806205chenqi <2056898264@qq.com>
2021-04-26 09:35:58 +0800

    rewrite to add book function

--- group-21/src/com/group21/www/dao/BookDao.java
+++ group-21/src/com/group21/www/dao/BookDao.java
@@ -1,4 +1,4 @@
-package com.group21.www.dao;
+﻿package com.group21.www.dao;
 
 import com.group21.www.utils.DBconnection;
 import com.group21.www.bean.Book;
@@ -15,9 +15,49 @@ public class BookDao {
         return null;
     }
 
-
-    public boolean addBook(Book book) {
-        return false;
+    /**
+     * 添加图书
+     *
+     * @param bookName   图书名
+     * @param authorName 作者名
+     * @return 增加图书是否成功
+     * @author 陈棋
+     */
+    public boolean addBook(Book book){
+        String addBookStr = "insert into book(bookname,authorname,state) values(?,?,?)";
+        Connection connection = DBconnection.getConnection();
+        PreparedStatement preparedStatement = null;
+        boolean result = false;
+        try {
+            preparedStatement = connection.prepareStatement(addBookStr);
+            preparedStatement.setString(1, book.getBookName());
+            preparedStatement.setString(2, book.getAuthorName());
+            preparedStatement.setBoolean(3,book.isState());
+            int res = preparedStatement.executeUpdate();
+            if (res > 0) {
+                result = true;
+            }else {
+                result = false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
     }
 
 
