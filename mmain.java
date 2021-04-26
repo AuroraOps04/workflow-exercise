5b566e8144b0a8ed2a14c79d58c1bf013b2d78f9
yy-hm-0806 <78125789+yy-hm-0806@users.noreply.github.com>
2021-04-26 14:52:57 +0800

    Update mmain.java

--- mmain.java
+++ mmain.java
@@ -19,24 +19,7 @@ public class mmain {
     }
     // 补全方法，想好返回值，输入参数，方法名,挑自己的部分写
      //1.图书查看功能,
-    public void select(book[] books) {
-        System.out.println("请输入书名进行查询：");
-        Scanner sc = new Scanner(System.in);
-        String name = sc.next();
-        if(name != null) {
 
-            for (int i = 0; i < books.length; i++) {
-                if (books[i].getBookname().equals(name)) {
-                    System.out.println("有此书籍！");
-                    System.out.println("书籍信息为：（书名-作者-价格-状态）");
-                    System.out.println(books[i].getBookname() + "   "+books[i].getAuth() + "   "+books[i].getPrice() + "    "+books[i].getIsStatue());
-                    break;
-                }
-                System.out.println("暂时没有此书！");
-                break;
-            }
-        }
-    }
     //高效使人的7个习惯
     // 2.图书增加功能
     // 3.图书删除功能
