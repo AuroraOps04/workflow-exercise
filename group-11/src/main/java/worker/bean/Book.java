3c95ab19785afc00c33fcfccd43ef4510eb4a887
xcsykb <82797175+xcsykb@users.noreply.github.com>
2021-05-09 21:39:57 +0800

    Add files via upload

---
+++ group-11/src/main/java/worker/bean/Book.java
@@ -0,0 +1,68 @@
+package worker.bean;
+
+public class Book {
+    private Integer id;
+
+    private String name;
+
+    private Integer isbn;
+
+    private String author;
+
+    private Double price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Integer isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", isbn=").append(isbn);
+        sb.append(", author=").append(author);
+        sb.append(", price=").append(price);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ group-11/src/main/java/worker/bean/Recommend.java
@@ -0,0 +1,35 @@
+package worker.bean;
+
+public class Recommend {
+    private String name;
+
+    private String author;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", name=").append(name);
+        sb.append(", author=").append(author);
+        sb.append("]");
+        return sb.toString();
+    }
+}
